24698c2d8325daa44268bbdcb39c915a9d301dba
SeulGi Jeong <sllki1@naver.com>
2017-12-28 20:28:39 +0900

    Spring Mybatis

--- srping_04_jdbc/src/main/java/part03_template/MemDaoImp.java
+++ srping_04_jdbc/src/main/java/part03_template/MemDaoImp.java
@@ -1,12 +1,8 @@
 package part03_template;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import com.sun.rowset.internal.Row;
+import org.mybatis.spring.SqlSessionTemplate;
 
 /*
  * Template 클래스
@@ -22,89 +18,45 @@ import com.sun.rowset.internal.Row;
  */
 
 public class MemDaoImp implements MemDAO {
-	private JdbcTemplate jdbcTemplate;
+
+	private SqlSessionTemplate sqlSession;
 
 	public MemDaoImp() {
 
 	}
 
-	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
+	public void setSqlSession(SqlSessionTemplate sqlSession) {
+		this.sqlSession = sqlSession;
 	}
 
-	@Override
 	public List<MemDTO> list() {
-		String sql = "select * from mem order by num desc";
-		List<MemDTO> list = jdbcTemplate.query(sql, new RowMapper<MemDTO>() { // 얘는 매핑할 값이 없어서 new 한것임
-			@Override
-			public MemDTO mapRow(ResultSet rs, int arg1) throws SQLException {
-				MemDTO dto = new MemDTO();
-				dto.setNum(rs.getInt("num"));
-				dto.setName(rs.getString("name"));
-				dto.setAge(rs.getInt("age"));
-				dto.setLoc(rs.getString("loc"));
-				return dto;
-			}
-		});
-		return list;
+
+		return sqlSession.selectList("mem.all");
 	}
 
-	@Override
 	public void insertMethod(MemDTO dto) {
-		String sql = "insert into mem values(mem_num_seq.nextval,?,?,?)";
-		Object[] args = new Object[] { dto.getName(), dto.getAge(), dto.getLoc() };
-
-		jdbcTemplate.update(sql, args);// 첫번째는 쿼리문, 그다음은 물음표에 들어갈 값 = 배열
+		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void updateMethod(MemDTO dto) {
-		String sql = "update mem set name=? where num=?";
-		Object[] args = new Object[] { dto.getName(), dto.getNum() };
+		// TODO Auto-generated method stub
 
-		jdbcTemplate.update(sql, args);// 첫번째는 쿼리문, 그다음은 물음표에 들어갈 값 = 배열
 	}
 
-	@Override
 	public void deleteMethod(int num) {
-		String sql = "delete from mem where num=?";
-		Object[] args = new Object[] { num };
-		jdbcTemplate.update(sql, args);
+		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public MemDTO one(int num) {
 		// TODO Auto-generated method stub
-		String sql = "select * from mem where num=?";
-		Object[] args = new Object[] { num };
-		// 이 결과값을 리턴하는거니까 return dt 안해도됨 , 바로 리턴 가능
-		return jdbcTemplate.queryForObject(sql, args, new RowMapper<MemDTO>() {
-			@Override
-			public MemDTO mapRow(ResultSet rs, int arg1) throws SQLException {
-				MemDTO dto = new MemDTO();
-				dto.setNum(rs.getInt("num"));
-				dto.setName(rs.getString("name"));
-				dto.setAge(rs.getInt("age"));
-				dto.setLoc(rs.getString("loc"));
-				return dto;
-			}
-		});
+		return null;
 	}
 
-	@Override
 	public int countMethod() {
-		String sql = "select count(*) from mem"; // 이렇게 함수일때는 인덱스로 해야 편함
-
-		return jdbcTemplate.queryForObject(sql, new RowMapper<Integer>() {
-
-			@Override
-			public Integer mapRow(ResultSet rs, int arg1) throws SQLException {
-				return rs.getInt(1);
-			}
-
-		});
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
